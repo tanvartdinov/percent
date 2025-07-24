c5a1ac492bc09138de8fe8a08b32875a00064b86
Timur Anvartdinov <t.anvartdinov@life-stream.tv>
2025-07-24 18:49:13 +0300

    jococo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 50, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 50;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
